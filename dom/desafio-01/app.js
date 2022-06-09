6d20ec3a0d4035eedaf267e857404805f940f7d4
Erik Gomes <erik.contato15@gmail.com>
2022-06-09 00:09:44 -0300

    Seção 2 - Interagindo com o DOM - Desafio 01.3

--- dom/desafio-01/app.js
+++ dom/desafio-01/app.js
@@ -3,5 +3,10 @@ new Vue({
     data: {
         nome: 'Erik Gomes',
         idade: '22'
+    },
+    methods: {
+        random01: function(){
+           return Math.random(0, 1);
+        }
     }
 })
